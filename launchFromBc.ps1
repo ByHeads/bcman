cd42d7142ddb44b6e2a1c261ff8e6b0bd02f39d8
erikvk <erik.von.krusenstierna@gmail.com>
2023-09-12 22:18:14 +0200

    Update launchFromBc.ps1

--- launchFromBc.ps1
+++ launchFromBc.ps1
@@ -1,6 +1,10 @@
 # This is an example of a script that launches a hosted Broadcaster Manager from a Broadcaster
 
-$url = Read-Host "> Enter the URL of the Broadacster (beginning with http:// or https://)" # Or hardcode it if appropriate
+# If using http instead of https, you need to set the AllowUnencryptedAuthentication flag
+$PSDefaultParameterValues['Invoke-RestMethod:AllowUnencryptedAuthentication'] = $true
+
+$url = Read-Host "> Enter the URL of the Broadcaster" # Or hardcode it if appropriate
 $apiKey = Read-Host "> Enter the API key to use" -AsSecureString
 $scriptText = irm "$url/api/bcman" -Cr ([PSCredential]::new("any", $apiKey))
-icm ([scriptblock]::Create($scriptText)) -Arg $url, $apiKey
+$scriptBlock = [scriptblock]::Create($scriptText)
+icm $scriptBlock -Arg $url, $apiKey
