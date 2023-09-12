ddaa49f6671495513dcfdc18e5cf396694225513
erikvk <erik.von.krusenstierna@gmail.com>
2023-09-12 23:33:10 +0200

    Update bcman.ps1

--- bcman.ps1
+++ bcman.ps1
@@ -21,7 +21,7 @@ Write-Host
 #region Setup Broadcaster connection
 function Try-Url
 {
-    param($url)
+    param($url, $critical)
     try {
         $options = irm $url -Method "OPTIONS" -TimeoutSec 5
         if (($options.Status -eq "success") -and ($options.Data[0].Resource -eq "RESTable.AvailableResource")) {
@@ -44,20 +44,10 @@ function Get-BroadcasterUrl
     if ($instr) { $instr = " $instr" }
     $input = Read-Host "> Enter the URL or hostname of the Broadcaster$instr"
     $input = $input.Trim()
-    if ( $input.StartsWith("@")) {
-        # Use input as-is
-        $input = $input.SubString(1)
-    }
+    if ( $input.StartsWith("@")) { $input = $input.SubString(1) }
     elseif (!$input.StartsWith("http")) {
-        # Build from a partial URL or conventional hostname
-        if ( $input.Contains(".")) {
-            # It's a partial URL
-            $input = "https://$input"
-        }
-        else {
-            # It's a hostname
-            $input = "https://broadcaster.$input.heads-api.com"
-        }
+        if ( $input.Contains(".")) { $input = "https://$input" }
+        else { $input = "https://broadcaster.$input.heads-api.com" }
     }
     if (!$input.EndsWith("/api")) {
         $input += "/api"
@@ -67,11 +57,49 @@ function Get-BroadcasterUrl
         Write-Host "Invalid URI format. Try again."
         return Get-BroadcasterUrl
     }
-
     if (Try-Url $input) {
         return $input
     }
     return Get-BroadcasterUrl $instr
+}
+
+function Get-BroadcasterUrl-Ism
+{
+    $input = Read-Host "> Enter the URL or hostname of the Broadcaster (or 'enter' to use this Broadcaster)"
+    $input = $input.Trim()
+    if ($input -eq "") {
+        Write-Host "> Using URL $bc"
+        return $bc
+    }
+    if ( $input.StartsWith("@")) {
+        $input = $input.SubString(1)
+    }
+    elseif (!$input.StartsWith("http")) {
+        if ( $input.Contains(".")) { $input = "https://$input" }
+        else { $input = "https://broadcaster.$input.heads-api.com" }
+    }
+    if (!$input.EndsWith("/api")) {
+        $input += "/api"
+    }
+    $r = $null
+    if (![System.Uri]::TryCreate($input, 'Absolute', [ref]$r)) {
+        Write-Host "Invalid URI format. Try again."
+        return Get-BroadcasterUrl
+    }
+    if ( $input.StartsWith("http://")) {
+        Write-Host "> You are using an unencrypted Broadcaster connection. Use Ctrl+C to abort..." -ForegroundColor Yellow
+        $PSDefaultParameterValues['Invoke-RestMethod:AllowUnencryptedAuthentication'] = $true
+    }
+    try {
+        $options = irm $input -Method "OPTIONS" -TimeoutSec 5
+        if (($options.Status -eq "success") -and ($options.Data[0].Resource -eq "RESTable.AvailableResource")) {
+            Write-Host "That Broadcaster exists! 🎉" -ForegroundColor Green
+            return $input
+        }
+    }
+    catch { }
+    Write-Host "Warning: Could not verify if a Broadcaster exists at $input" -ForegroundColor Yellow
+    return $input
 }
 
 function Get-ApiKeyCredentials
@@ -170,14 +198,13 @@ function Get-Batch
 function Yes
 {
     param($message)
-    $val = Read-Host "$message (yes/no/cancel)"
+    $val = Read-Host "$message (yes/no)"
     $val = $val.Trim()
-    if ($val -eq "cancel") { return $null }
     if ($val -eq "yes") { return $true }
     if ($val -eq "no") { return $false }
     if ($val -eq "y") { return $true }
     if ($val -eq "n") { return $false }
-    Write-Host "Invalid value, expected yes, no or cancel"
+    Write-Host "Invalid value, expected yes or no"
     return Yes $message
 }
 function Num
@@ -1145,12 +1172,59 @@ $remoteDeploymentCommands = @(
     Command = "ISM"
     Description = "Starts Install Script Maker"
     Action = {
-        Write-Host "Now starting ISM. Use " -NoNewLine
-        Write-Host -ForegroundColor:Yellow "Ctrl+C" -NoNewLine
-        Write-Host " to return to Broadcaster Manager"
-        irm raw.githubusercontent.com/byheads/util/main/ism | iex
         Write-Host
-        Write-Host "Returning to Broadcaster Manager..."
+        Write-Host "This tool will help create a Broadcaster install script!" -ForegroundColor Green
+        Write-Host
+        $bcUrl = Get-BroadcasterUrl-Ism
+        $token = Read-Host "> Now enter the install token (or 'enter' for a new 7 day token)" -MaskInput
+        if ($token -eq "") {
+            $token = irm "$bcUrl/InstallToken" @getSettingsRaw | % { $_.Token }
+        }
+        $uris = @()
+        if (Yes "> Should we first uninstall existing client software, if present?") {
+            if (Yes "--> Also uninstall legacy (SUS/RA) client software?") {
+                $uris += "'uninstall.legacy'"
+            }
+            $uris += "'uninstall.all'"
+        }
+        if (Yes "> Install Receiver?") {
+            $uris += "'install/p=Receiver'"
+        }
+        $csa = $false
+        if (Yes "> Install WpfClient?") {
+            $part = "p=WpfClient"
+            if (Yes "--> Install as a manual client?") {
+                $label = Label
+                $installPath = [System.Uri]::EscapeDataString("C:\ProgramData\Heads\$label")
+                $part += "&installPath=$installPath"
+                $shortcutLabel = [System.Uri]::EscapeDataString("Heads Retail - $label")
+                $part += "&shortcutLabel=$shortcutLabel"
+            }
+            $part += "&usePosServer=" + (Yes "--> Connect client to local POS Server?")
+            $part += "&useArchiveServer=" + (Yes "--> Connect client to central Archive Server?")
+            $uris += "'install/$part'"
+        }
+        elseif (Yes "> Install CustomerServiceApplication?") {
+            $uris += "'install/p=CustomerServiceApplication'"
+            $csa = $true
+        }
+        if (!$csa -and (Yes "> Install POS Server?")) {
+            $part = "p=PosServer"
+            $part += "&createDump=" + (Yes "--> Create a dump of an existing POS-server?")
+            $part += "&collation=" + (Collation "--> Enter database collation, e.g. sv-SE")
+            $part += "&databaseImageSize=" + (Num "--> Enter database image size in MB (or enter for 1024)" 1024)
+            $part += "&databaseLogSize=" + (Num "--> Enter database log size in MB (or enter for 1024)" 1024)
+            $uris += "'install/$part'"
+        }
+        $arr = $uris | Join-String -Separator ","
+        Write-Host
+        Write-Host "# Here's your install script! Run it in PowerShell as administrator on a client computer:"
+        Write-Host
+        Write-Host "$arr|%{try{irm('$bcUrl/'+`$_)-He @{Authorization='Bearer $token'}|iex}catch{echo `$_;return}};"
+        Write-Host
+        Write-Host "# End of script"
+        Write-Host
+        Write-Host "Returning to Broadcaster Manager..." -ForegroundColor Yellow
         Write-Host
     }
 }
@@ -1319,11 +1393,7 @@ $remoteDeploymentCommands = @(
         [string[]]$workstationIds = Get-WorkstationIds
         Write-Host "> Selected these workstations for reset:"
         $workstationIds | Out-Host
-        $closeDayJournal = Yes "> Should we close relevant day journals before resetting these workstations?"
-        if ($closeDayJournal -eq $null) {
-            return
-        }
-        if ($closeDayJournal) {
+        if (Yes "> Should we close relevant day journals before resetting these workstations?") {
             $posUser = Read-Host "> Enter the user name to call the POS-server APIs with when closing the day journals or 'cancel' to cancel"
             $posUser = $posUser.Trim()
             if ($posUser -ieq "cancel") {
